4a8746bf44ef1e5dcac11c520e6a0bcf910a0591
motcom <motti234@qct.so-net.ne.jp>
2023-11-29 16:36:43 +0900

    first_commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+        startInfo.FileName = "shutdown";  
+        startInfo.Arguments = "-r -t 0";  
+        startInfo.RedirectStandardOutput = false; 
+        startInfo.UseShellExecute = false; 
+        startInfo.CreateNoWindow = true; 
+
+        Process? process = Process.Start(startInfo);
+        if ( process is not null ) {
+           process.Start();
+        }
+    }
+}
